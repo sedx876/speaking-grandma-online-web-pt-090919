e50057451495dbd9209aaebab3bca9bd4b412e07
Sharon Watkins <swatkins876@gmail.com>
2019-09-10 16:53:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase)
+#def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
@@ -9,7 +9,7 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   end 
 
-  end
+  #end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
